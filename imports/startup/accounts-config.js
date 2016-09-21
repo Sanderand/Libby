765b9e9342bc41010bd1e63a24b37552341f95ce
Andre Sander <andre--sander@web.de>
2016-09-21 12:30:30 +1200

    refac + design work

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -10,15 +10,15 @@ Accounts.ui.config({
   passwordSignupFields: 'USERNAME_ONLY',
 });
 
-Accounts.onLogin(() => { // TODO refactor
-  const currentPath = FlowRouter._current.path;
-  const appDefaultPath = '/app/';
+Accounts.onLogin(() => {
+  const authPath = '/app/';
   const publicPath = '/public/';
-  const isAuthPath = (currentPath.indexOf(appDefaultPath) !== -1);
+  const currentPath = FlowRouter._current.path;
+  const isAuthPath = (currentPath.indexOf(authPath) !== -1);
   const isPublicPath = (currentPath.indexOf(publicPath) !== -1);
 
-  if (!isAuthPath && !isPublicPath) { // force-redirect to auth path
-    FlowRouter.go(appDefaultPath);
+  if (!isAuthPath && !isPublicPath) {
+    FlowRouter.go(authPath);
   }
 });
 
